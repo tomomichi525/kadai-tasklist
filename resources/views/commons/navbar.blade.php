2edafbf151dac6d35290f77218758976f0cb871b
tomomi.kashiwagi <tomo525@hotmail.com>
2018-06-05 04:56:23 +0000

    bootstrap to tasklist

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    
+<header>
+    <nav class="navbar navbar-inverse navbar-static-top".
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> 
+                    <span class="sr-only">toggle navigation</span>
+                    <span class="icon-bar"></span>    
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span>
+                </button>   
+                <a class="navbar-brand" href="/">Tasklist</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-left">
+                    <li>{!! link_to_route('tasks.create', '新規タスクの投稿') !!}</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
